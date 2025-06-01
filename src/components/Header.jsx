aba9bf4c37b1c50a6e2aef9b5b0ceb40e58956e6
Nvtai <buiblue123@gmail.com>
2025-06-01 19:52:58 +0700

    chore: config deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import logo from "./../assets/images/logo.png";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import {
   HiHome,
   HiMagnifyingGlass,
-  HiStar,
   HiPlayCircle,
+  HiStar,
   HiTv,
 } from "react-icons/hi2";
-import { HiPlus, HiDotsVertical } from "react-icons/hi";
+import logo from "./../assets/images/logo.png";
 import HeaderItem from "./HeaderItem";
 
 function Header() {
@@ -18,21 +17,37 @@ function Header() {
     { name: "ORIGINALS", icon: HiStar },
     { name: "MOVIES", icon: HiPlayCircle },
     { name: "SERIES", icon: HiTv },
-    // { name: "MORE", icon: HiDotsVertical },
   ];
 
   return (
     <div className="flex justify-between items-center p-8">
-      <div className="flex gap-8">
+      <div className="flex gap-4 md:gap-10 lg:gap-14">
         <img
           src={logo}
           alt="Disney"
-          className="w-[80px] md:w-[115px] object-cover mr-12"
+          className="w-[80px] md:w-[115px] object-cover mr-4 md:mr-12"
         />
 
-        {menu.map((item) => (
-          <HeaderItem name={item.name} Icon={item.icon} />
-        ))}
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+
+        <div className="flex md:hidden gap-4">
+          {menu.map(
+            (item, index) => index < 3 && <HeaderItem Icon={item.icon} />
+          )}
+          <div className="flex items-center" onClick={() => document.querySelector("#dropdown").classList.toggle("hidden")}>
+            <HeaderItem Icon={HiDotsVertical} />
+          </div>
+          <div className="hidden absolute mt-12 ml-20 bg-black rounded-lg p-4 flex flex-col gap-2 border border-gray-700" id="dropdown">
+            {menu.map(
+              (item, index) =>
+                index > 2 && <HeaderItem name={item.name} Icon={item.icon} />
+            )}
+          </div>
+        </div>
       </div>
 
       <div>
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function HeaderItem({ name, Icon }) {
   return (
-    <div className="text-white flex items-center  gap-3 text-[18px] font-semibold cursor-pointer">
+    <div className="text-white flex items-center  gap-3 text-[18px] font-semibold cursor-pointer hover:underline underline-offset-6">
       <Icon />
-      <h2 className="hover:underline underline-offset-8">{name}</h2>
+      <h2 className="">{name}</h2>
     </div>
   );
 }
