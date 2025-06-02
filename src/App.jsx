c818b4d7df7b0e402b1215dab60c3a3f5811cb64
Nvtai <buiblue123@gmail.com>
2025-06-02 21:56:01 +0700

    feat: animated production house card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Slider from "./components/Slider";
 import Header from "./components/Header";
+import ProductionHouse from "./components/ProductionHouse";
 
 function App() {
   return (
     <>
       <Header />
-      <Slider/>
+      <Slider />
+      <ProductionHouse />
     </>
   );
 }
